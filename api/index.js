602ec6c6bba51a15f9a63fe3064a81006c62b90b
srinath <srinath23022005@gmail.com>
2025-03-26 16:47:35 +0530

    UPDATE MODELS

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect("mongodb+srv://saiestate:saiestate%40230205@saiestate.bxr4r.mongodb.net/?retryWrites=true&w=majority&appName=SaiEstate")
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000,() => {
-    console.log('listening on port 3000!!!!!!!!!!!!!');
+app.listen(3000, () => {
+    console.log('Listening on port 3000!');
 });
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-blue-300 shadow-md">
+    <header className="bg-blue-500 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-blue-500">React</span>
-            <span className="text-blue-700">Estate</span>
+            <span className="text-blue-700">React</span>
+            <span className="text-blue-900">Estate</span>
           </h1>
         </Link>
         <form className="bg-blue-200 p-2 rounded-lg flex items-center ">
@@ -18,9 +18,9 @@ export default function Header() {
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
           />
-          <BiSearchAlt2 className="font-bold text-blue-700" />
+          <BiSearchAlt2 className="font-bold text-blue-900" />
         </form>
-        <ul className="flex gap-4 text-blue-800">
+        <ul className="flex gap-4 text-blue-900">
           <Link to="/">
             <li className="hidden sm:inline hover:underline">Home</li>
           </Link>
